35209fe62a7b28bfdea90a24fac0d1369fb3d4a0
Joao Antonio <ja.lourenco@outlook.com>
2024-02-08 15:54:25 -0300

    style: styling team page

--- src/layout/index.tsx
+++ src/layout/index.tsx
@@ -8,103 +8,103 @@ import { Link } from "react-router-dom";
 import { Outlet } from "react-router-dom";
 
 import {
-	ArrowLeftToLine,
-	ArrowRightToLine,
-	Box,
-	Columns3,
-	Handshake,
-	Home,
-	LogOut,
-	UserCog,
-	Users,
+  ArrowLeftToLine,
+  ArrowRightToLine,
+  Box,
+  Columns3,
+  Handshake,
+  Home,
+  LogOut,
+  UserCog,
+  Users,
 } from "lucide-react";
 
 interface LayoutProps {
-	pageName: string;
+  pageName: string;
 }
 
 export function Layout({ pageName }: LayoutProps) {
-	const [isSidebarOpen, setIsSidebarOpen] = useState(true);
+  const [isSidebarOpen, setIsSidebarOpen] = useState(true);
 
-	const menuItems = [
-		{ name: "Início", icon: <Home />, path: "/home" },
-		{ name: "Painel de Leads", icon: <Columns3 />, path: "/panel" },
-		{ name: "Clientes", icon: <Users />, path: "/customer" },
-		{ name: "Produtos", icon: <Box />, path: "/product" },
-		{ name: "Equipe", icon: <Handshake />, path: "/teams" },
-		{ name: "Usuários", icon: <UserCog />, path: "/users" },
-		{ name: "Sair", icon: <LogOut />, path: "/" },
-	];
+  const menuItems = [
+    { name: "Início", icon: <Home />, path: "/home" },
+    { name: "Painel de Leads", icon: <Columns3 />, path: "/panel" },
+    { name: "Clientes", icon: <Users />, path: "/customer" },
+    { name: "Produtos", icon: <Box />, path: "/product" },
+    { name: "Equipe", icon: <Handshake />, path: "/team" },
+    { name: "Usuários", icon: <UserCog />, path: "/users" },
+    { name: "Sair", icon: <LogOut />, path: "/" },
+  ];
 
-	function handleSidebar() {
-		setIsSidebarOpen((state) => !state);
-	}
+  function handleSidebar() {
+    setIsSidebarOpen((state) => !state);
+  }
 
-	function Sidebar() {
-		return isSidebarOpen ? (
-			<aside className="border-r-2 px-2 flex flex-col gap-2 w-52 relative">
-				<h2 className="text-2xl font-bold text-center my-10">Nord CRM</h2>
+  function Sidebar() {
+    return isSidebarOpen ? (
+      <aside className="border-r-2 px-2 flex flex-col gap-2 w-52 relative">
+        <h2 className="text-2xl font-bold text-center my-10">Nord CRM</h2>
 
-				{menuItems.map(({ name, icon, path }, index) => (
-					<Link to={path} key={index}>
-						<Button className="gap-2" variant="ghost">
-							{icon}
-							{name}
-						</Button>
-					</Link>
-				))}
+        {menuItems.map(({ name, icon, path }, index) => (
+          <Link to={path} key={index}>
+            <Button className="gap-2" variant="ghost">
+              {icon}
+              {name}
+            </Button>
+          </Link>
+        ))}
 
-				<div className="absolute bottom-5 right-2">
-					<Button variant="ghost" size={"sm"} onClick={() => handleSidebar()}>
-						<ArrowLeftToLine />
-					</Button>
-				</div>
-			</aside>
-		) : (
-			<aside className="border-r-2 px-2 flex flex-col items-center gap-2 w-15 relative">
-				<h2 className="text-2xl font-bold text-center my-10">NR</h2>
+        <div className="absolute bottom-5 right-2">
+          <Button variant="ghost" size={"sm"} onClick={() => handleSidebar()}>
+            <ArrowLeftToLine />
+          </Button>
+        </div>
+      </aside>
+    ) : (
+      <aside className="border-r-2 px-2 flex flex-col items-center gap-2 w-15 relative">
+        <h2 className="text-2xl font-bold text-center my-10">NR</h2>
 
-				{menuItems.map(({ icon, path }, index) => (
-					<Link to={path} key={index}>
-						<Button variant="ghost" size={"sm"}>
-							{icon}
-						</Button>
-					</Link>
-				))}
+        {menuItems.map(({ icon, path }, index) => (
+          <Link to={path} key={index}>
+            <Button variant="ghost" size={"sm"}>
+              {icon}
+            </Button>
+          </Link>
+        ))}
 
-				<div className="absolute bottom-5">
-					<Button variant="ghost" size={"icon"} onClick={() => handleSidebar()}>
-						<ArrowRightToLine />
-					</Button>
-				</div>
-			</aside>
-		);
-	}
+        <div className="absolute bottom-5">
+          <Button variant="ghost" size={"icon"} onClick={() => handleSidebar()}>
+            <ArrowRightToLine />
+          </Button>
+        </div>
+      </aside>
+    );
+  }
 
-	return (
-		<div className="h-dvh flex gap-x-10">
-			<Sidebar />
+  return (
+    <div className="h-dvh flex gap-x-10">
+      <Sidebar />
 
-			<main className="w-full pt-10 pr-5">
-				<div className="w-full flex items-center justify-between">
-					<h1 className="text-3xl font-bold">{pageName.toUpperCase()}</h1>
+      <main className="w-full pt-10 pr-5">
+        <div className="w-full flex items-center justify-between">
+          <h1 className="text-3xl font-bold">{pageName.toUpperCase()}</h1>
 
-					<div className="flex items-center gap-5">
-						<h3>Bem Vindo (a), Bruna!</h3>
+          <div className="flex items-center gap-5">
+            <h3>Bem Vindo (a), Bruna!</h3>
 
-						<Avatar>
-							<AvatarImage src="" />
-							<AvatarFallback>NO</AvatarFallback>
-						</Avatar>
+            <Avatar>
+              <AvatarImage src="" />
+              <AvatarFallback>NO</AvatarFallback>
+            </Avatar>
 
-						<ModeToggle />
-					</div>
-				</div>
+            <ModeToggle />
+          </div>
+        </div>
 
-				<div className="pt-10">
-					<Outlet />
-				</div>
-			</main>
-		</div>
-	);
+        <div className="pt-10">
+          <Outlet />
+        </div>
+      </main>
+    </div>
+  );
 }
--- src/routes/root.tsx
+++ src/routes/root.tsx
@@ -5,54 +5,65 @@ import { Home } from "@/pages/Home";
 import { Panel } from "@/pages/Panel";
 import { Customer } from "@/pages/Customer";
 import { Product } from "@/pages/Product";
+import { Team } from "@/pages/Team";
 import { ErrorPage } from "@/pages/ErrorPage";
 
 import { Layout } from "@/layout";
 
 export const router = createBrowserRouter([
-	{
-		path: "/home",
-		element: <Layout pageName="INÍCIO" />,
-		children: [
-			{
-				path: "/home",
-				element: <Home />,
-			},
-		],
-	},
-	{
-		path: "/panel",
-		element: <Layout pageName="PAINEL DE LEADS" />,
-		children: [
-			{
-				path: "/panel",
-				element: <Panel />,
-			},
-		],
-	},
-	{
-		path: "/customer",
-		element: <Layout pageName="CLIENTES" />,
-		children: [
-			{
-				path: "/customer",
-				element: <Customer />,
-			},
-		],
-	},
-	{
-		path: "/product",
-		element: <Layout pageName="PRODUTOS" />,
-		children: [
-			{
-				path: "/product",
-				element: <Product />,
-			},
-		],
-	},
-	{
-		path: "/",
-		element: <Login />,
-		errorElement: <ErrorPage />,
-	},
+  {
+    path: "/home",
+    element: <Layout pageName="INÍCIO" />,
+    children: [
+      {
+        path: "/home",
+        element: <Home />,
+      },
+    ],
+  },
+  {
+    path: "/panel",
+    element: <Layout pageName="PAINEL DE LEADS" />,
+    children: [
+      {
+        path: "/panel",
+        element: <Panel />,
+      },
+    ],
+  },
+  {
+    path: "/customer",
+    element: <Layout pageName="CLIENTES" />,
+    children: [
+      {
+        path: "/customer",
+        element: <Customer />,
+      },
+    ],
+  },
+  {
+    path: "/product",
+    element: <Layout pageName="PRODUTOS" />,
+    children: [
+      {
+        path: "/product",
+        element: <Product />,
+      },
+    ],
+  },
+  {
+    path: "/team",
+    element: <Layout pageName="Equipe" />,
+    children: [
+      {
+        path: "/team",
+        element: <Team />,
+      },
+    ],
+  },
+  {
+    path: "/",
+    element: <Login />,
+    errorElement: <ErrorPage />,
+  },
 ]);
