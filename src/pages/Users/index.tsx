25d678aff83acdca258196b52349383fe396744b
Joao <ja.lourenco@outlook.com>
2024-02-08 22:03:54 -0300

    refactor: adjusting table view routes

---
+++ src/pages/Users/index.tsx
@@ -0,0 +1,65 @@
+import {
+	Table,
+	TableBody,
+	TableCell,
+	TableHead,
+	TableHeader,
+	TableRow,
+} from "@/components/ui/table";
+import { Input } from "@/components/ui/input";
+
+import { Button } from "@/components/ui/button";
+
+import { Pencil, Plus, Search } from "lucide-react";
+
+export function Users() {
+	return (
+		<>
+			<section className="flex items-center justify-between mb-5">
+				<div className="flex items-center">
+					<Input type="text" placeholder="Pesquisar..." className="w-52 mr-2" />
+
+					<Button>
+						<Search />
+					</Button>
+				</div>
+
+				<Button>
+					<Plus /> Adicionar
+				</Button>
+			</section>
+
+			<Table>
+				<TableHeader>
+					<TableRow>
+						<TableHead>Usuário</TableHead>
+						<TableHead>Nome</TableHead>
+						<TableHead>Editar</TableHead>
+					</TableRow>
+				</TableHeader>
+
+				<TableBody>
+					<TableRow>
+						<TableCell>raul.osorio</TableCell>
+						<TableCell>Raul Osório</TableCell>
+						<TableCell>
+							<Button>
+								<Pencil />
+							</Button>
+						</TableCell>
+					</TableRow>
+
+					<TableRow>
+						<TableCell>tallis.marques</TableCell>
+						<TableCell>Tallis Marques</TableCell>
+						<TableCell>
+							<Button>
+								<Pencil />
+							</Button>
+						</TableCell>
+					</TableRow>
+				</TableBody>
+			</Table>
+		</>
+	);
+}
--- src/routes/root.tsx
+++ src/routes/root.tsx
@@ -3,78 +3,98 @@ import { createBrowserRouter } from "react-router-dom";
 import { Login } from "@/pages/Login";
 import { Home } from "@/pages/Home";
 import { Panel } from "@/pages/Panel";
-import { Customer } from "@/pages/Customer";
-import { Product } from "@/pages/Product";
-import { Team } from "@/pages/Team";
+import { Customers } from "@/pages/Customers";
+import { Products } from "@/pages/Products";
+import { Teams } from "@/pages/Teams";
+import { Users } from "@/pages/Users";
 import { ErrorPage } from "@/pages/ErrorPage";
 
 import { Layout } from "@/layout";
-import { User } from "@/pages/User";
 
 export const router = createBrowserRouter([
-  {
-    path: "/home",
-    element: <Layout pageName="INÍCIO" />,
-    children: [
-      {
-        path: "/home",
-        element: <Home />,
-      },
-    ],
-  },
-  {
-    path: "/panel",
-    element: <Layout pageName="PAINEL DE LEADS" />,
-    children: [
-      {
-        path: "/panel",
-        element: <Panel />,
-      },
-    ],
-  },
-  {
-    path: "/customer",
-    element: <Layout pageName="CLIENTES" />,
-    children: [
-      {
-        path: "/customer",
-        element: <Customer />,
-      },
-    ],
-  },
-  {
-    path: "/product",
-    element: <Layout pageName="PRODUTOS" />,
-    children: [
-      {
-        path: "/product",
-        element: <Product />,
-      },
-    ],
-  },
-  {
-    path: "/team",
-    element: <Layout pageName="Equipe" />,
-    children: [
-      {
-        path: "/team",
-        element: <Team />,
-      },
-    ],
-  },
-  {
-    path: "/user",
-    element: <Layout pageName="USUÁRIOS" />,
-    children: [
-      {
-        path: "/user",
-        element: <User />,
-      },
-    ],
-  },
-  {
-    path: "/",
-    element: <Login />,
-    errorElement: <ErrorPage />,
-  },
+	{
+		path: "/home",
+		element: <Layout pageName="INÍCIO" />,
+		children: [
+			{
+				path: "/home",
+				element: <Home />,
+			},
+		],
+	},
+	{
+		path: "/panel",
+		element: <Layout pageName="PAINEL DE LEADS" />,
+		children: [
+			{
+				path: "/panel",
+				element: <Panel />,
+			},
+		],
+	},
+	{
+		path: "/customers",
+		element: <Layout pageName="CLIENTES" />,
+		children: [
+			{
+				index: true,
+				path: "/customers",
+				element: <Customers />,
+			},
+			{
+				path: "/customers/:id",
+				element: <h1>TESTE CLIENTES</h1>,
+			},
+		],
+	},
+	{
+		path: "/products",
+		element: <Layout pageName="PRODUTOS" />,
+		children: [
+			{
+				index: true,
+				path: "/products",
+				element: <Products />,
+			},
+			{
+				path: "/products/:id",
+				element: <h1>TESTE PRODUTOS</h1>,
+			},
+		],
+	},
+	{
+		path: "/teams",
+		element: <Layout pageName="EQUIPE" />,
+		children: [
+			{
+				index: true,
+				path: "/teams",
+				element: <Teams />,
+			},
+			{
+				path: "/teams/:id",
+				element: <h1>TESTE EQUIPE</h1>,
+			},
+		],
+	},
+	{
+		path: "/users",
+		element: <Layout pageName="USUÁRIOS" />,
+		children: [
+			{
+				index: true,
+				path: "/users",
+				element: <Users />,
+			},
+			{
+				path: "/users/:id",
+				element: <h1>TESTE USUÁRIOS</h1>,
+			},
+		],
+	},
+	{
+		path: "/",
+		element: <Login />,
+		errorElement: <ErrorPage />,
+	},
 ]);
