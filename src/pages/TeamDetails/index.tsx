8adc276c0c3d585f4568a44869899aefe64a0954
Joao <ja.lourenco@outlook.com>
2024-02-13 21:35:41 -0300

    style: new style structure for team details page

--- src/pages/TeamDetails/index.tsx
+++ src/pages/TeamDetails/index.tsx
@@ -1,4 +1,4 @@
-import { useState } from "react";
+import { useEffect, useState } from "react";
 
 import { Button } from "@/components/ui/button";
 import { Input } from "@/components/ui/input";
@@ -6,44 +6,65 @@ import { Label } from "@/components/ui/label";
 import { Modal } from "@/components/Modal";
 import { DialogClose } from "@/components/ui/dialog";
 import { ScrollArea } from "@/components/ui/scroll-area";
-import {
-	Form,
-	FormControl,
-	FormField,
-	FormItem,
-	FormLabel,
-} from "@/components/ui/form";
 
 import { useForm } from "react-hook-form";
 import { z } from "zod";
-import { CornerDownLeft, Save, UserX } from "lucide-react";
+import { api } from "@/services/api";
+
+import {
+	CornerDownLeft,
+	Save,
+	Search,
+	UserMinus,
+	UserPlus,
+	X,
+} from "lucide-react";
 
 const teamFormSchema = z.object({
 	name: z.string(),
-	members: z.array(z.string()).refine((value) => value.some((item) => item)),
 });
 
 type TeamType = z.infer<typeof teamFormSchema>;
 
 export function TeamDetails() {
-	const [users, setUsers] = useState([
-		{ id: "1", username: "user1" },
-		{ id: "2", username: "user2" },
-		{ id: "3", username: "user3" },
-		{ id: "4", username: "user4" },
-		{ id: "5", username: "user5" },
-		{ id: "6", username: "user6" },
-		{ id: "7", username: "user7" },
-		{ id: "8", username: "user8" },
-		{ id: "9", username: "user9" },
-		{ id: "10", username: "user10" },
-	]);
+	const [users, setUsers] = useState([]);
+	const [members, setMembers] = useState([]);
 
 	const form = useForm<TeamType>();
 	const { register, handleSubmit } = form;
 
 	function submit(data: TeamType) {
 		console.log("product", data);
+	}
+
+	async function getUsers() {
+		try {
+			const { data } = await api.get(
+				"https://jsonplaceholder.typicode.com/users"
+			);
+
+			const newData = data.map((item: any) => {
+				return { ...item, checked: false };
+			});
+			setUsers(newData);
+		} catch (e) {
+			console.log("getUsers Error: ", e);
+		}
+	}
+
+	async function getMembers() {
+		try {
+			const { data } = await api.get(
+				"https://jsonplaceholder.typicode.com/users"
+			);
+
+			const newData = data.map((item: any) => {
+				return { ...item, checked: false };
+			});
+			setMembers(newData);
+		} catch (e) {
+			console.log("getUsers Error: ", e);
+		}
 	}
 
 	function RemoveModalFooter() {
@@ -54,56 +75,103 @@ export function TeamDetails() {
 					Cancelar
 				</DialogClose>
 
-				<Button className="h-10 col-span-1 bg-red-500 dark:text-white hover:dark:text-black">
-					<UserX className="mr-2" />
-					Confirmar
+				<Button className="h-10 bg-red-500 dark:text-white hover:dark:text-black">
+					<X className="mr-2" />
+					Remover
 				</Button>
 			</div>
 		);
 	}
 
+	useEffect(() => {
+		getUsers();
+		getMembers();
+	}, []);
+
 	return (
-		<Form {...form}>
-			<form
-				onSubmit={handleSubmit(submit)}
-				className="w-2/6 flex flex-col gap-5"
-			>
-				<div className="">
-					<Label htmlFor="name">Nome da Equipe</Label>
-					<Input id="name" type="text" {...register("name")} />
+		<form onSubmit={handleSubmit(submit)} className="w-2/6 flex flex-col gap-5">
+			<div className="">
+				<Label htmlFor="name">Nome da Equipe</Label>
+				<Input id="name" type="text" {...register("name")} />
+			</div>
+
+			<h2 className="text-xl -mb-3">Usuários</h2>
+			<section className="p-5 border-2 rounded-b-md">
+				<div className="flex items-center gap-3">
+					<Input
+						type="text"
+						id="search"
+						name="search"
+						placeholder="Pesquisar"
+					/>
+
+					<Button>
+						<Search />
+					</Button>
 				</div>
 
-				<section className="p-5 border-2 rounded-b-md">
-					<ScrollArea className="h-52">
-						{users.map(({ id, username }) => (
-							<div key={id} className="flex items-center gap-3">
-								<Label htmlFor={id}>{username}</Label>
-							</div>
-						))}
-					</ScrollArea>
-				</section>
+				<ScrollArea className="h-52 mt-5">
+					{users.map(({ id, name }) => (
+						<div
+							key={id}
+							className="flex items-center justify-between gap-3 mt-5 pr-5"
+						>
+							<p>{name}</p>
+							<Button variant="outline">
+								<UserPlus />
+							</Button>
+						</div>
+					))}
+				</ScrollArea>
+			</section>
 
-				<section className="flex items items-center justify-between gap-3">
-					<Modal
-						title="ATENÇÃO"
-						content="Deseja realmente remover este registro?"
-						footer={<RemoveModalFooter />}
-					>
-						<Button type="button" className="h-10 w-full mt-5 col-span-1">
-							<UserX className="mr-2" />
-							Remover
-						</Button>
-					</Modal>
+			<h2 className="text-xl -mb-3">Membros</h2>
+			<section className="p-5 border-2 rounded-b-md">
+				<div className="flex items-center gap-3">
+					<Input
+						type="text"
+						id="search"
+						name="search"
+						placeholder="Pesquisar"
+					/>
 
-					<Button
-						type="submit"
-						className="h-10 w-full mt-5 col-span-1 bg-green-500"
-					>
-						<Save className="mr-2" />
-						Salvar
+					<Button>
+						<Search />
 					</Button>
-				</section>
-			</form>
-		</Form>
+				</div>
+
+				<ScrollArea className="h-52 mt-5">
+					{members.map(({ id, name }) => (
+						<div
+							key={id}
+							className="flex items-center justify-between gap-3 mt-5 pr-5"
+						>
+							<p>{name}</p>
+							<Button variant="outline">
+								<UserMinus />
+							</Button>
+						</div>
+					))}
+				</ScrollArea>
+			</section>
+
+			<section className="flex items items-center justify-between gap-3 mt-5">
+				<Modal
+					title="ATENÇÃO"
+					content="Deseja realmente remover este registro?"
+					footer={<RemoveModalFooter />}
+				>
+					<Button type="button" variant="outline" className="h-10 w-full">
+						<X className="mr-2" />
+						Remover
+					</Button>
+				</Modal>
+
+				<Button type="submit" className="h-10 w-full bg-green-500">
+					<Save className="mr-2" />
+					Salvar
+				</Button>
+			</section>
+		</form>
 	);
 }
