105a149756e6be762961a6e10911d2c1f4b85869
João Antonio <ja.lourenco@outlook.com>
2024-07-27 18:21:04 -0300

    style: new sidebar style

--- src/layout/index.tsx
+++ src/layout/index.tsx
@@ -5,11 +5,11 @@ import { Avatar, AvatarFallback, AvatarImage } from "@/components/ui/avatar";
 import { ModeToggle } from "@/components/ui/mode-toggle";
 import { Separator } from "@/components/ui/separator";
 
-import { Link } from "react-router-dom";
-import { Outlet } from "react-router-dom";
+import { Outlet, Link, useNavigate } from "react-router-dom";
 
 import {
 	AlignStartHorizontal,
+	ArrowLeft,
 	ArrowLeftToLine,
 	ArrowRightToLine,
 	Box,
@@ -28,6 +28,8 @@ interface LayoutProps {
 export function Layout({ pageName }: LayoutProps) {
 	const [isSidebarOpen, setIsSidebarOpen] = useState(true);
 
+	const navigate = useNavigate();
+
 	const menuItems = [
 		{ name: "Início", icon: <Home />, path: "/home" },
 		{
@@ -43,45 +45,39 @@ export function Layout({ pageName }: LayoutProps) {
 		{ name: "Sair", icon: <LogOut />, path: "/" },
 	];
 
+	const username = localStorage.getItem("username");
+
 	function handleSidebar() {
 		setIsSidebarOpen((state) => !state);
 	}
 
 	function Sidebar() {
-		return isSidebarOpen ? (
-			<aside className="border-r-2 px-2 flex flex-col gap-2 w-52 relative">
-				<h2 className="text-2xl font-bold text-center my-10">Nord CRM</h2>
+		return (
+			<aside
+				className={`border-r-2 px-2 flex flex-col gap-2 relative ${
+					isSidebarOpen ? "w-52" : "w-22"
+				}`}
+			>
+				<h2 className="text-2xl font-bold text-center my-10">
+					{isSidebarOpen ? "Nord CRM" : "NR"}
+				</h2>
 
 				{menuItems.map(({ name, icon, path }, index) => (
-					<Link to={path} key={index}>
-						<Button className="gap-2" variant="ghost">
+					<Button variant="ghost">
+						<Link
+							to={path}
+							key={index}
+							className="flex items-center gap-2 w-full"
+						>
 							{icon}
-							{name}
-						</Button>
-					</Link>
+							{isSidebarOpen && name}
+						</Link>
+					</Button>
 				))}
 
 				<div className="absolute bottom-5 right-2">
 					<Button variant="ghost" size={"sm"} onClick={() => handleSidebar()}>
-						<ArrowLeftToLine />
-					</Button>
-				</div>
-			</aside>
-		) : (
-			<aside className="border-r-2 px-2 flex flex-col items-center gap-2 w-15 relative">
-				<h2 className="text-2xl font-bold text-center my-10">NR</h2>
-
-				{menuItems.map(({ icon, path }, index) => (
-					<Link to={path} key={index}>
-						<Button variant="ghost" size={"sm"}>
-							{icon}
-						</Button>
-					</Link>
-				))}
-
-				<div className="absolute bottom-5">
-					<Button variant="ghost" size={"icon"} onClick={() => handleSidebar()}>
-						<ArrowRightToLine />
+						{isSidebarOpen ? <ArrowLeftToLine /> : <ArrowRightToLine />}
 					</Button>
 				</div>
 			</aside>
@@ -94,14 +90,25 @@ export function Layout({ pageName }: LayoutProps) {
 
 			<main className="w-full pt-10 pr-10">
 				<div className="w-full flex items-center justify-between">
-					<h1 className="text-3xl font-bold">{pageName.toUpperCase()}</h1>
+					<div className="flex items-center gap-5">
+						<Button
+							variant="ghost"
+							className="w-full"
+							onClick={() => navigate(-1)}
+						>
+							<ArrowLeft />
+						</Button>
+						<h1 className="text-3xl font-bold">{pageName.toUpperCase()}</h1>
+					</div>
 
 					<div className="flex items-center gap-5">
-						<h3>Bem Vindo (a), Bruna!</h3>
+						<h3>Bem Vindo (a), {username ? username.toUpperCase() : "NR"}!</h3>
 
 						<Avatar>
 							<AvatarImage src="" />
-							<AvatarFallback>NO</AvatarFallback>
+							<AvatarFallback>
+								{username ? username.substring(0, 2).toUpperCase() : "NR"}
+							</AvatarFallback>
 						</Avatar>
 
 						<ModeToggle />
